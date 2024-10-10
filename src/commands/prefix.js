1c83ef46e8134c86ed57f9eb87edba590c0aa1c7
Mateo-Chatbot <mateochatbot@gmail.com>
2024-10-10 21:13:09 +0100

    Update prefix.js

--- src/commands/prefix.js
+++ src/commands/prefix.js
@@ -8,14 +8,14 @@ module.exports = {
     try {
       const myPrefix = 'my prefix is: \nonline: \n⟩ type ${prefix}help to see my commands.\n⟩ Nexus-MD: thank you for choosing us.';
 
-      if (args.[0] === 'prefix') {
+      if (args[0] === 'prefix') {
         await message.reply(myPrefix);
-      };
+      },
 
       logger.info('prefix: ${prefix}');
     } catch (error) {
       logger.info('error oops');
-      return message.reply('error my bad');
+      return message.reply('error, my bad');
     },
   },
 };
