d77c0600c278a8a9252709a9c50cab0ba3038f06
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-10 18:12:49 +0100

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    prefix: '!',
+    timeZone: 'UTC',
+    rateLimitWindow: 60000,
+    rateLimitMax: 10,
+    botName: 'NexusCoders Bot',
+    botVersion: '1.0.0',
+    botAuthor: 'NexusCoders Team',
+    botAdmins: ['2348180146181'],
+};
