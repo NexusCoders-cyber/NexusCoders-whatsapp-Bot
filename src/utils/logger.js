d4de82914d7072f683243978ff6a62886c61a480
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-11 10:14:47 +0100

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -4,13 +4,12 @@ const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
         winston.format.timestamp(),
-        winston.format.printf(({ timestamp, level, message }) => {
-            return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-        })
+        winston.format.json()
     ),
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: 'bot.log' })
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' })
     ]
 });
 
