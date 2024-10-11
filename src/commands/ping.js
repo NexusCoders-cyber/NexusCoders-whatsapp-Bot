38eb71059b49600a2476b33f511cd4aab17846ec
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-11 10:17:35 +0100

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'ping',
-    execute: async (msg, args) => {
+    description: 'Ping command to check bot responsiveness',
+    execute: async (msg) => {
         const start = Date.now();
         const reply = await msg.reply('Pong!');
         const end = Date.now();
