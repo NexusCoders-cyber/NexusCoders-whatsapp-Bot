f4520c5241182bc178ae50f1ad8a4bd4f0a42ee9
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-11 22:48:18 +0100

    Delete src directory

--- src/commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'Ping!',
-    async execute(sock, msg, args) {
-        await sock.sendMessage(msg.key.remoteJid, { text: 'Pong!' });
-    },
-};
--- src/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    prefix: '!',
-    ownerNumber: '2348180146181',
-    mongodbUri: process.env.MONGODB_URI || 'mongodb+srv://mateochatbot:xdtL2bYQ9eV3CeXM@gerald.r2hjy.mongodb.net/',
-    sessionId: process.env.SESSION_ID || 'nexuscoders-session'
-};
