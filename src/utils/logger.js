208ae72eb2cc0bd14aabef6c5cae69b06d2f42e5
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-11 23:01:34 +0100

    Create logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,19 @@
+const winston = require('winston');
+const path = require('path');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: path.join(__dirname, '..', '..', 'logs', 'error.log'), level: 'error' }),
+        new winston.transports.File({ filename: path.join(__dirname, '..', '..', 'logs', 'combined.log') })
+    ]
+});
+
+module.exports = logger;
