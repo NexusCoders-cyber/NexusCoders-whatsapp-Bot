904b29df4a911bdc0205707593c572c25587ee5f
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-11 09:16:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -69,13 +69,12 @@ async function main() {
     });
 
     process.on('unhandledRejection', (reason, promise) => {
-        logger.error('Unhandled Rejection at:', promise, 'reason:', reason);
-    });
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
 
-    process.on('uncaughtException', (error) => {
-        logger.error('Uncaught Exception:', error);
-        process.exit(1);
-    });
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+});
 
     process.on('SIGINT', async () => {
         logger.info('NexusCoders Bot shutting down...');
