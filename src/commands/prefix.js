3f9d68f10b394ddbf2d849a3f14ca111b8a69530
Mateo-Chatbot <mateochatbot@gmail.com>
2024-10-11 07:03:00 +0100

    Update prefix.js

--- src/commands/prefix.js
+++ src/commands/prefix.js
@@ -4,7 +4,7 @@ const prefix = global.('../config.js').prefix;
 module.exports = {
   name: 'prefix',
   description: '',
-  async execute(global, client, message, args) {
+  async execute(global, event, client, message, args) {
     try {
       const myPrefix = 'my prefix is: ${prefix}\nonline: \n⟩ type ${prefix}help to see my commands.\n⟩ Nexus-MD: thank you for choosing us.';
 
