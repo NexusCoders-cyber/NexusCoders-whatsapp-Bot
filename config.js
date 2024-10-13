eda06215869f4c3dddc5721f82c06782ac0df01d
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-13 08:38:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,26 +1,9 @@
-/* ---> THE NEXUS CODERS WHATSAPP BOT <---
-1. YOU CAN CHANGE THE OWNERS NUMBER TO YOURS.
-2. ADD YOUR SESSION ID.
-3. DO NOT CHANGE THE MONGODB URL IF YOU DON'T HAVE ANY IT MIGHT LEAD TO ERROR.
-4. IF YOU HAVE ANY ERROR CONTACT NEXUS TEAM WITH ISSUES.
-*/
-
-const sessionId = proc.env.sessionId || 'Nexus-session';
-const mongodbUrl = ''; /* notes: do not change else it might lead to error but you can also add your if you have*/
-const adminIds = [
-  "your_number",
-  "your other number",
-  "leave this space blank",
-];
-const botName = proc.env.botName || 'Nexus-MD';
-const prefix = proc.env.prefix || '!'; //configure this if you set '' blank bot will have no prefix.
-const mode = 'public';
+require('dotenv').config();
 
 module.exports = {
-  sessionId,
-  mongodbUrl,
-  adminIds,
-  botName,
-  prefix,
-  mode,
+    ownerNumber: process.env.OWNER_NUMBER || '2347075663318',
+    prefix: process.env.PREFIX || '!',
+    mongodbUri: process.env.MONGODB_URI || 'mongodb+srv://mateochatbot:xdtL2bYQ9eV3CeXM@gerald.r2hjy.mongodb.net/',
+    port: process.env.PORT || 3000,
+    sessionDir: './auth_info_baileys',
 };
